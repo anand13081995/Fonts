a4542642c66046899bd0bb9c2d39fc9d615a3a5d
anand13081995 <anandsahani213@gmail.com>
2024-05-04 23:47:41 +0530

    font icons used

---
+++ src/components/FontIcons/FontIcons.jsx
@@ -0,0 +1,157 @@
+const FontIcons = ()=> {
+    return(
+        <div>
+            <div>
+                <section style={{display:"flex", justifyContent:'center', marginTop:'2%'}}>
+
+                    <div style={{width:'150px', height:'180px', textAlign:'center'}}>
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                        <path strokeLinecap="round" strokeLinejoin="round" d="M17.982 18.725A7.488 7.488 0 0 0 12 15.75a7.488 7.488 0 0 0-5.982 2.975m11.963 0a9 9 0 1 0-11.963 0m11.963 0A8.966 8.966 0 0 1 12 21a8.966 8.966 0 0 1-5.982-2.275M15 9.75a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
+                    </svg>
+
+                        <h6>PROFILE</h6>
+                    </div>
+                    <div style={{height:'180px', textAlign:'center'}}>
+                        <h1>JESSICA GREENE</h1>
+                        <h6>Freelance Writer</h6>
+                    </div>
+                    <div style={{width:'400px', height:'180px'}}>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci dolore voluptate reiciendis quidem recusandae, quis eos autem eveniet debitis quae.</p>
+                        <h6>Email : aaaaaaaaaaaa@xyz.com</h6>
+                        <h6>Website : mywebsite.com</h6>
+                    </div>
+                </section>
+
+
+
+
+
+                <section style={{display:"flex", justifyContent:'center', gap:'98px', marginTop:'2%'}}>
+                    <div style={{width:'150px',height:'150px', textAlign:'center'}}>
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                            <path strokeLinecap="round" strokeLinejoin="round" d="M20.25 14.15v4.25c0 1.094-.787 2.036-1.872 2.18-2.087.277-4.216.42-6.378.42s-4.291-.143-6.378-.42c-1.085-.144-1.872-1.086-1.872-2.18v-4.25m16.5 0a2.18 2.18 0 0 0 .75-1.661V8.706c0-1.081-.768-2.015-1.837-2.175a48.114 48.114 0 0 0-3.413-.387m4.5 8.006c-.194.165-.42.295-.673.38A23.978 23.978 0 0 1 12 15.75c-2.648 0-5.195-.429-7.577-1.22a2.016 2.016 0 0 1-.673-.38m0 0A2.18 2.18 0 0 1 3 12.489V8.706c0-1.081.768-2.015 1.837-2.175a48.111 48.111 0 0 1 3.413-.387m7.5 0V5.25A2.25 2.25 0 0 0 13.5 3h-3a2.25 2.25 0 0 0-2.25 2.25v.894m7.5 0a48.667 48.667 0 0 0-7.5 0M12 12.75h.008v.008H12v-.008Z" />
+                        </svg>
+                        
+                        <h6>WORK</h6>
+                    </div>
+
+                    <div style={{display:'flex', flexDirection:'column', textAlign:'center'}}>
+
+                        <div style={{display:'flex', gap:'45px'}}>
+                            <h6>Present</h6>
+                            <div>
+                                <h6>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos, maiores?</h6>
+                                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+                            </div>
+                        </div>
+
+                        <div style={{display:'flex', gap:'45px'}}>
+                            <h6>2016</h6>
+                            <div>
+                                <h6>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos, maiores?</h6>
+                                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+                            </div>
+                        </div>
+
+                        <div style={{display:'flex', gap:'45px'}}>
+                            <h6>2013-2015</h6>
+                            <div>
+                                <h6>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos, maiores?</h6>
+                                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+                            </div>
+                        </div>
+                    </div>
+                </section>
+
+
+
+                <section style={{display:"flex", justifyContent:'center', textAlign:'center', paddingLeft:'320px', marginTop:'2%'}}>
+                    <div >
+                        <svg style={{width:'150px',height:'150px', textAlign:'center'}} xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                            <path strokeLinecap="round" strokeLinejoin="round" d="M9 12h3.75M9 15h3.75M9 18h3.75m3 .75H18a2.25 2.25 0 0 0 2.25-2.25V6.108c0-1.135-.845-2.098-1.976-2.192a48.424 48.424 0 0 0-1.123-.08m-5.801 0c-.065.21-.1.433-.1.664 0 .414.336.75.75.75h4.5a.75.75 0 0 0 .75-.75 2.25 2.25 0 0 0-.1-.664m-5.8 0A2.251 2.251 0 0 1 13.5 2.25H15c1.012 0 1.867.668 2.15 1.586m-5.8 0c-.376.023-.75.05-1.124.08C9.095 4.01 8.25 4.973 8.25 6.108V8.25m0 0H4.875c-.621 0-1.125.504-1.125 1.125v11.25c0 .621.504 1.125 1.125 1.125h9.75c.621 0 1.125-.504 1.125-1.125V9.375c0-.621-.504-1.125-1.125-1.125H8.25ZM6.75 12h.008v.008H6.75V12Zm0 3h.008v.008H6.75V15Zm0 3h.008v.008H6.75V18Z" />
+                        </svg>
+                        
+                        <h6>Skills & Samples</h6>
+                    </div>
+
+                    <div style={{width:'400px', padding:'20px'}}>
+                        <div style={{display:'flex', gap:'20px', textAlign:'center'}}>
+                            <h6>Writing</h6>
+                            <p>Lorem, ipsum dolor.</p>
+                        </div>
+
+                        <div style={{display:'flex', gap:'20px', textAlign:'center'}}>
+                            <h6>Editing</h6>
+                            <p>Lorem, ipsum dolor.</p>
+                        </div>
+
+
+                        <div style={{display:'flex', gap:'20px', textAlign:'center'}}>
+                            <h6>Social Media</h6>
+                            <p>Lorem, ipsum dolor.</p>
+                        </div>
+
+                        <div style={{display:'flex', gap:'20px', textAlign:'center'}}>
+                            <h6>Researching</h6>
+                            <p>Lorem, ipsum dolor.</p>
+                        </div>
+
+
+                        <div style={{display:'flex', gap:'20px', textAlign:'center'}}>
+                            <h6>SEO</h6>
+                            <p>Lorem, ipsum dolor.</p>
+                        </div>
+                    </div>
+
+                    <div style={{padding:'40px'}}>
+                    <div style={{display:'flex', gap:'15px'}}>
+                        <h6>Zapier</h6>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+                    </div>
+
+                    <div style={{display:'flex', gap:'15px'}}>
+                        <h6>Spoke</h6>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+                    </div>
+
+
+                    <div style={{display:'flex', gap:'15px'}}>
+                        <h6>nDash</h6>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+                    </div>
+                    </div>
+
+                    
+
+                </section>
+
+
+                <section style={{display:'flex', justifyContent:'center', gap:'45px', marginTop:'3%'}}>
+                    <svg style={{width:'100px', height:'100px'}} xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                        <path strokeLinecap="round" strokeLinejoin="round" d="M12 18v-5.25m0 0a6.01 6.01 0 0 0 1.5-.189m-1.5.189a6.01 6.01 0 0 1-1.5-.189m3.75 7.478a12.06 12.06 0 0 1-4.5 0m3.75 2.383a14.406 14.406 0 0 1-3 0M14.25 18v-.192c0-.983.658-1.823 1.508-2.316a7.5 7.5 0 1 0-7.517 0c.85.493 1.509 1.333 1.509 2.316V18" />
+                        </svg>
+                   
+                    <svg style={{width:'100px', height:'100px'}} xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                        <path strokeLinecap="round" strokeLinejoin="round" d="M20.25 14.15v4.25c0 1.094-.787 2.036-1.872 2.18-2.087.277-4.216.42-6.378.42s-4.291-.143-6.378-.42c-1.085-.144-1.872-1.086-1.872-2.18v-4.25m16.5 0a2.18 2.18 0 0 0 .75-1.661V8.706c0-1.081-.768-2.015-1.837-2.175a48.114 48.114 0 0 0-3.413-.387m4.5 8.006c-.194.165-.42.295-.673.38A23.978 23.978 0 0 1 12 15.75c-2.648 0-5.195-.429-7.577-1.22a2.016 2.016 0 0 1-.673-.38m0 0A2.18 2.18 0 0 1 3 12.489V8.706c0-1.081.768-2.015 1.837-2.175a48.111 48.111 0 0 1 3.413-.387m7.5 0V5.25A2.25 2.25 0 0 0 13.5 3h-3a2.25 2.25 0 0 0-2.25 2.25v.894m7.5 0a48.667 48.667 0 0 0-7.5 0M12 12.75h.008v.008H12v-.008Z" />
+                    </svg>
+                   
+                    <svg style={{width:'100px', height:'100px'}} xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                        <path strokeLinecap="round" strokeLinejoin="round" d="M19.114 5.636a9 9 0 0 1 0 12.728M16.463 8.288a5.25 5.25 0 0 1 0 7.424M6.75 8.25l4.72-4.72a.75.75 0 0 1 1.28.53v15.88a.75.75 0 0 1-1.28.53l-4.72-4.72H4.51c-.88 0-1.704-.507-1.938-1.354A9.009 9.009 0 0 1 2.25 12c0-.83.112-1.633.322-2.396C2.806 8.756 3.63 8.25 4.51 8.25H6.75Z" />
+                    </svg>
+                    
+                    <svg style={{width:'100px', height:'100px'}} xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                        <path strokeLinecap="round" strokeLinejoin="round" d="M6 20.25h12m-7.5-3v3m3-3v3m-10.125-3h17.25c.621 0 1.125-.504 1.125-1.125V4.875c0-.621-.504-1.125-1.125-1.125H3.375c-.621 0-1.125.504-1.125 1.125v11.25c0 .621.504 1.125 1.125 1.125Z" />
+                    </svg>
+                    
+                    <svg style={{width:'100px', height:'100px'}} xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                        <path strokeLinecap="round" strokeLinejoin="round" d="M12 18v-5.25m0 0a6.01 6.01 0 0 0 1.5-.189m-1.5.189a6.01 6.01 0 0 1-1.5-.189m3.75 7.478a12.06 12.06 0 0 1-4.5 0m3.75 2.383a14.406 14.406 0 0 1-3 0M14.25 18v-.192c0-.983.658-1.823 1.508-2.316a7.5 7.5 0 1 0-7.517 0c.85.493 1.509 1.333 1.509 2.316V18" />
+                    </svg>
+                </section>
+
+
+            </div>
+        </div>
+    )
+}
+
+export default FontIcons;
---
+++ src/main.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+
+import './index.css'
+import FontIcons from './components/FontIcons/FontIcons'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <FontIcons />
+  </React.StrictMode>,
+)
